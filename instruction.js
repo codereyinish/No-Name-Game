1b45249a60c4f413cd397f5ec5589ad47c77cf7e
Manish <86160294+codereyinish@users.noreply.github.com>
2024-04-17 00:16:38 -0400

    "Feat: Add JS code to direct to Main Game Part"
    Make User read the instruction first and then direct user to main game part on clicking the Start Game Button

---
+++ instruction.js
@@ -0,0 +1,9 @@
+
+const startBtn = document.querySelector("#startGameBtn");
+document.addEventListener("DOMContentLoaded", function()
+{
+    startBtn.addEventListener("click", ()=>
+    {
+        window.location.href = "MainGame.html";
+    })
+})
